f4b0da503d831f8f5216c72ad24a6d0dffbdb250
Amelia Brown <amelia.brown389@gmail.com>
2018-03-22 10:52:17 -0700

    basic farms photos routes structure

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,23 @@ import { renderRoutes } from 'react-router-config'
 import Root from 'containers/root'
 
 import Home from './home'
+import Farms from './farms'
+import Photos from './photos'
 
 export default renderRoutes([
   {
     component: Root,
     routes: [
+      {
+        component: Farms,
+        key: 'farms',
+        path: '/farms'
+      },
+      {
+        component: Photos,
+        key: 'photos',
+        path: '/photos'
+      },
       {
         component: Home,
         key: 'home',
